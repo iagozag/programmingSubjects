233fd538df0317d04b378ad474967c4ebc2fa755
iagozag <iagozag@gmail.com>
2024-06-21 19:55:37 -0300

    update

--- PC/lista3/p9.cpp
+++ PC/lista3/p9.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define rep(i,x,n) for(auto i=x;i<n;i++)
-#define repr(i,n,x) for(auto i=n;i>=x;i--)
-#define forr(x, v) for(auto& x: v)
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define rep(i, x, n) for (auto i = x; i < n; i++)
+#define repr(i, n, x) for (auto i = n; i >= x; i--)
+#define forr(x, v) for (auto& x : v)
 #define all(a) (a).begin(), (a).end()
 #define sz(a) (int)(a.size())
 #define endl '\n'
@@ -14,18 +14,18 @@ using namespace std;
 #define eb emplace_back
 
 typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
-void no(){ cout << "NO" << endl; }
-void yes(){ cout << "YES" << endl; }
+void no() { cout << "NO" << endl; }
+void yes() { cout << "YES" << endl; }
 
-const int MAX = 2e5+10, MOD = 1e9+7;
+const int MAX = 2e5 + 10, MOD = 1e9 + 7;
 
 struct FlowEdge {
     int v, u;
@@ -109,63 +109,67 @@ struct Dinic {
     }
 };
 
-int lcs(vi& a, vi& b){
-    int na = a.size(), mb = b.size();
-    vector<vi> memo(na+1, vi(mb+1, -1)); 
+vector<vector<int>> memo(100, vector<int>(100));
+vector<vector<int>> vis(100, vector<int>(100, -1));
+int cnt = 0;
 
-    function<int(int, int)> dp = [&](int i, int j){
-        if(i == 0 or j == 0) return 0;
+int lcs(vector<int>& person, vector<int>& toy, int i, int j) {
+    if (i == sz(person) or j == sz(toy))
+        return 0;
+    int& v = vis[i][j];
+    int& p = memo[i][j];
+    if (v >= cnt)
+        return p;
+    v = cnt;
 
-        int& p = memo[i][j];
-        if(p != -1) return p;
-
-        if(a[i-1] == b[j-1]) return p = dp(i-1, j-1)+1;
-        return p = max(dp(i-1, j), dp(i, j-1));
-    };
-
-    return dp(na, mb);
+    if (person[i] == toy[j])
+        return p = 1 + lcs(person, toy, i + 1, j + 1);
+    return p = max(lcs(person, toy, i + 1, j), lcs(person, toy, i, j + 1));
 }
 
-void solve(int n, int m){
+void solve(int n, int m) {
     vector<string> f(n), t(m);
     forr(x, f) cin >> x;
     forr(x, t) cin >> x;
 
     vector<vi> fr(n), to(m);
-    rep(i, 0, n){
+    rep(i, 0, n) {
         map<char, int> mp;
-        forr(x, f[i]){
-            if(!mp.count(x)) mp[x] = (x-'A'), mp[x]++;
+        forr(x, f[i]) {
+            if (!mp.count(x)) mp[x] = (x - 'A') + 1;
             fr[i].eb(mp[x]), mp[x] += 26;
         }
     }
-    rep(i, 0, m){
+    rep(i, 0, m) {
         map<char, int> mp;
-        forr(x, t[i]){
-            if(!mp.count(x)) mp[x] = (x-'A'), mp[x]++;
+        forr(x, t[i]) {
+            if (!mp.count(x)) mp[x] = (x - 'A') + 1;
             to[i].eb(mp[x]), mp[x] += 26;
         }
     }
 
     vector<vector<bool>> can(m, vector<bool>(n));
-    rep(i, 0, n) rep(j, 0, m)
-        can[j][i] = !((fr[i].size()+to[i].size()-2*lcs(fr[i], to[i]))%5);
+    rep(i, 0, n) rep(j, 0, m) {
+        can[j][i] = !((fr[i].size() + to[j].size() - 2 * lcs(fr[i], to[j], 0, 0)) % 5);
+        cnt++;
+    }
 
-    Dinic F(2+n+m, 0, n+m+1);
-    rep(i, 1, m+1) F.add_edge(0, i, 1);
-    rep(i, 1, m+1)
-        rep(j, 0, n) if(can[i-1][j])
-            F.add_edge(i, m+1+j, 1);
+    Dinic F(2 + n + m, 0, n + m + 1);
+    rep(i, 1, m + 1) F.add_edge(0, i, 1);
+    rep(i, 1, m + 1)
+        rep(j, 0, n) if (can[i - 1][j])
+            F.add_edge(i, m + 1 + j, 1);
 
-    rep(i, m+1, m+1+n) F.add_edge(i, m+1+n, 1);
+    rep(i, m + 1, m + 1 + n) F.add_edge(i, m + 1 + n, 1);
 
-    double ans = ((1.0*F.flow())/(1.0*n))*100.0;
+    double ans = ((1.0 * F.flow()) / (1.0 * n)) * 100.0;
     cout << "P = " << fixed << setprecision(2) << ans << endl;
 }
 
-int main(){ _
+int main() { _
     int n, m;
-    while(cin >> n >> m and n) solve(n, m);
+    while (cin >> n >> m and n) solve(n, m);
 
-    exit(0);
+    return 0;
 }
+
